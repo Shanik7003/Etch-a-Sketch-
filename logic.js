528e0c7e735b42e995ec96cf1182e000447cb8de
Shanik Montes de Oca <shanik0307@gmail.com>
2025-09-24 20:51:39 +0200

    popup terminado

--- logic.js
+++ logic.js
@@ -1,25 +1,5 @@
 let grid = document.querySelector(".grid"); // para que funcione grid tiene que estar en colum
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell"); // 👈 le pongo clase
-        row.appendChild(cell);
-    }
-
-    grid.appendChild(row);
-}
-
-// let cuadritos  = document.querySelectorAll(".cell");
-// cuadritos.forEach(cuadro => {
-//   cuadro.addEventListener("mouseover", () => {
-//     cuadro.style.backgroundColor = "black"; // ejemplo
-//   });
-// }); esta es la misma forma de hacer lo de abajo pero aqui creo 16x16 eventListeners y en la otra no, la de abajo es mejor, me la dio GPTo
-
 grid.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("cell")) {
     e.target.style.backgroundColor = "black";
@@ -46,5 +26,26 @@ okButton.addEventListener("click", () => {
     }
     else{
         popup.style.display = "none";
+        createGrid(number);
+        grid.style.display = "flex";
     }
 } );
+
+function createGrid(num) {
+    //primero se debe verificar que el  numero sea valido, luego lo hago 
+    
+
+    for (let i = 0; i < num; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < num; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell"); // 👈 le pongo clase
+        row.appendChild(cell);
+    }
+
+    grid.appendChild(row);
+}
+
+}
